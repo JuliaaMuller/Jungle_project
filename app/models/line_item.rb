70c967a88fc81c95cbd390f5067fbbd3ce1d6f95
JuliaaMuller <julialm.muller@gmail.com>
2022-03-08 20:03:15 -0500

    fix the bug about money formatting

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -3,7 +3,7 @@ class LineItem < ActiveRecord::Base
   belongs_to :order
   belongs_to :product
 
-  monetize :item_price_cents, numericality: true
+  monetize :item__cents, numericality: true
   monetize :total_price_cents, numericality: true
 
 end
